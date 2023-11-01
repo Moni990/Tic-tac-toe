a77ee7780f46b15e15e0609e4ed9fad4b262ad6d
Moni <monihuangpro@gmail.com>
2023-10-31 23:40:21 -0700

    Final

--- logic.py
+++ logic.py
@@ -10,13 +10,20 @@ def make_empty_board():
         [None, None, None],
     ]
 
-
 def get_winner(board):
-    """Determines the winner of the given board.
-    Returns 'X', 'O', or None."""
-    return None  # FIXME
-
+    # Check rows, columns, and diagonals for a win
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0] is not None:
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i] and board[0][i] is not None:
+            return board[0][i]
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] is not None:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] is not None:
+        return board[0][2]
+    return None  # No winner yet
 
 def other_player(player):
-    """Given the character for a player, returns the other player."""
-    return "O"  # FIXME
+    return 'O' if player == 'X' else 'X'
+
+
