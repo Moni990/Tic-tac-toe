c6c16ee42d5287c6d9d9e76b78f55914674d4cc1
Moni <monihuangpro@gmail.com>
2023-11-21 22:03:13 -0800

    Assignment8 unit test for Tictactoe

--- tests.py
+++ tests.py
@@ -1,63 +1,48 @@
-# tests.py
-
 import unittest
+from unittest.mock import patch
 from logic import TicTacToe, HumanPlayer, ComputerPlayer
 
 class TestTicTacToe(unittest.TestCase):
+    
     def setUp(self):
         self.player1 = HumanPlayer('X')
         self.player2 = HumanPlayer('O')
         self.game = TicTacToe(self.player1, self.player2)
 
-    def test_initial_board_empty(self):
-        for row in self.game.board:
-            for cell in row:
-                self.assertEqual(cell, ' ')
-
-    def test_move(self):
-        self.game.board = [[' ', ' ', ' '],
-                           [' ', ' ', ' '],
-                           [' ', ' ', ' ']]
-        self.game.players[self.game.current_player].get_move = lambda board: (0, 0)
-        self.assertTrue(self.game.make_move())
-        self.assertEqual(self.game.board[0][0], 'X')
+    def test_init(self):
+        self.assertEqual(self.game.board, [[' ' for _ in range(3)] for _ in range(3)])
+        self.assertIs(self.game.players[0], self.player1)
+        self.assertIs(self.game.players[1], self.player2)
+        self.assertEqual(self.game.current_player, 0)
+        self.assertIsNone(self.game.winner)
 
     def test_switch_player(self):
-        self.game.players[self.game.current_player].get_move = lambda board: (0, 0)
-        self.game.make_move()
-        self.assertEqual(self.game.players[self.game.current_player].symbol, 'O')
+        self.game.switch_player()
+        self.assertEqual(self.game.current_player, 1)
 
-    def test_winner_row(self):
-        self.game.board = [['X', 'X', 'X'],
-                           [' ', ' ', ' '],
-                           [' ', ' ', ' ']]
-        self.assertTrue(self.game.check_winner(0, 0))
-        self.game.winner = 'X'  # 手动设置获胜者
-        self.assertEqual(self.game.get_winner(), 'X')
+    # Other tests for make_move, check_winner, etc.
 
-    def test_winner_diagonal(self):
-        self.game.board = [['X', ' ', ' '],
-                           [' ', 'X', ' '],
-                           [' ', ' ', 'X']]
-        self.assertTrue(self.game.check_winner(0, 0))
-        self.game.winner = 'X'  # 手动设置获胜者
-        self.assertEqual(self.game.get_winner(), 'X')
+class TestHumanPlayer(unittest.TestCase):
+    
+    def setUp(self):
+        self.player = HumanPlayer('X')
 
-    def test_draw(self):
-        self.game.board = [['X', 'O', 'X'],
-                           ['X', 'X', 'O'],
-                           ['O', 'X', 'O']]
-        self.assertTrue(self.game.is_draw())
-        self.assertIsNone(self.game.get_winner())
+    # Mock input for get_move
+    @patch('builtins.input', side_effect=['0', '0'])
+    def test_get_move(self, mock_input):
+        self.assertEqual(self.player.get_move([[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]), (0, 0))
 
-    def test_computer_player_move(self):
-        computer = ComputerPlayer('O')
-        self.game.board = [['X', 'X', ' '],
-                           [' ', ' ', ' '],
-                           [' ', ' ', ' ']]
-        empty_cells = [(i, j) for i in range(3) for j in range(3) if self.game.board[i][j] == ' ']
-        row, col = computer.get_move(self.game.board)
-        self.assertIn((row, col), empty_cells)
+    # Other tests for invalid inputs, etc.
+
+class TestComputerPlayer(unittest.TestCase):
+    
+    def setUp(self):
+        self.player = ComputerPlayer('O')
+
+    def test_get_move(self):
+        board = [['X', 'X', 'O'], ['O', 'X', 'X'], ['X', 'O', ' ']]
+        move = self.player.get_move(board)
+        self.assertIn(move, [(2, 2)])
 
 if __name__ == '__main__':
     unittest.main()
